ce9283ca22134880a165e0c8c14a00659ef566d4
Tiago Rinaldi <trinaldi@gmail.com>
2024-12-30 10:48:18 -0300

    remove typeorm

--- api/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { loginController } from '../controllers/loginController';
-
-const router = Router();
-
-router.post('/login', loginController);
-
-export default router;
-
