37409c04d9598e234a758f28b90eea78bcc73681
Tiago Rinaldi <trinaldi@gmail.com>
2025-01-07 17:03:42 -0300

    add expense component sketch

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
+import ExpenseList from './components/expense/ExpenseList'
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <ExpenseList />
     </>
   )
 }
