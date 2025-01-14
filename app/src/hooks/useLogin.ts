dd99e75860ea50d5ff97e2723f3abe9b84ceafe7
Tiago Rinaldi <trinaldi@gmail.com>
2025-01-14 00:22:13 -0300

    add login hook

---
+++ app/src/hooks/useLogin.ts
@@ -0,0 +1,44 @@
+import { useState } from "react"
+
+interface UseLoginResult {
+  login: (email: string, password: string) => Promise<void>;
+  error: string | null;
+  isLoading: boolean;
+}
+
+const useLogin = () : UseLoginResult => {
+  const [error, setError] = useState<string | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const login = async(email: string, password: string) => {
+    setIsLoading(true)
+    setError(null)
+
+    try {
+      const response = await fetch('http://localhost:3000/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        localStorage.setItem('token', data.token)
+      } else {
+        const data = await response.json()
+        setError(data.message || 'Login failed')
+      }
+    } catch (e) {
+      const error = e as Error
+      setError(error.message || 'An error ocorred')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return { login, error, isLoading }
+}
+
+export default useLogin
